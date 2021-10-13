26778a23f65cae1475865b5f5111e0fa4135ad8c
Anthony <anton09092002@gmail.com>
2021-10-14 00:34:06 +0300

    Fixed pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import React, { useState } from "react"; // React must be in scope when using JSX
-import { API_KEY } from "./constants";
+import { API_KEY, STARTUP_MESSAGE } from "./constants";
 import { APIResponse, InputEvent } from "./types";
 import MoviesContainer from "./components/MoviesContainer";
 
 export default function App(): JSX.Element {
+	/*
+		Pagination kinda breaks when, after initial search, you erase contents of the search box.
+		That's why I decided to make a persistent version of the search query,
+		so the pagination would work fine even if you change the search input value but not press search again
+	*/
+	const [noMoviesMessage, setNoMoviesMessage] = useState(STARTUP_MESSAGE);
 	const [searchQuery, setSearchQuery] = useState("");
+	const [persistentSearchQuery, setPersistentSearchQuery] = useState("");
 	const [movies, setMovies] = useState<APIResponse["Search"]>([]);
 	const [pages, setPages] = useState(0);
 
@@ -13,19 +20,42 @@ export default function App(): JSX.Element {
 		setSearchQuery(value);
 	};
 
+	const returnToStartupState = (): void => {
+		setPersistentSearchQuery("");
+		setNoMoviesMessage(STARTUP_MESSAGE);
+		setMovies([]);
+		setPages(0);
+	};
+
 	/*
 		Okay, hear me out. The response doesn't give you all movies, maximum ten movies at once.
 		I wanted to implement some recursive calls to fetch all movies at once,
 		but pagination turned out much easier to implement
 	*/
-	const handleSearch = async (page: number): Promise<void> => {
+	const handleSearch = async (query: string, page: number, saveQuery: boolean): Promise<void> => {
+		if (!query) {
+			returnToStartupState();
+			return;
+		}
 		const apiResponse = await fetch(
-			`http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchQuery}&page=${page}`
+			`http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}&page=${page}`
 		);
-		const { Search: results, totalResults, Response: response } = await apiResponse.json();
+		const {
+			Search: results,
+			totalResults,
+			Response: response,
+			Error: error,
+		} = await apiResponse.json();
 		if (response === "True") {
 			setMovies(results);
+			// A solution to beforementioned pagination breaking
+			if (saveQuery) {
+				setPersistentSearchQuery(searchQuery);
+			}
 			setPages(Math.ceil(+totalResults / 10));
+		} else {
+			returnToStartupState();
+			setNoMoviesMessage(error);
 		}
 	};
 
@@ -33,7 +63,7 @@ export default function App(): JSX.Element {
 		const buttons: JSX.Element[] = [];
 		for (let i = 0; i < pages; i++) {
 			buttons.push(
-				<button key={i} onClick={() => handleSearch(i)}>
+				<button key={i} onClick={() => handleSearch(persistentSearchQuery, i + 1, false)}>
 					{i + 1}
 				</button>
 			);
@@ -43,17 +73,30 @@ export default function App(): JSX.Element {
 
 	return (
 		<div>
-			<input type="text" id="movie-search-text" onChange={handleChange} value={searchQuery} />
+			<input
+				type="text"
+				id="movie-search-text"
+				onChange={handleChange}
+				value={searchQuery}
+				placeholder="Search for a movie"
+			/>
 			<input
 				type="button"
 				id="search-button"
-				onClick={() => handleSearch(1)}
+				onClick={() => handleSearch(searchQuery, 1, true)}
 				value="Search"
 			/>
 			<div>
-				{pages ? renderPagination(pages) : null}
-				<MoviesContainer movies={movies} />
-				{pages ? renderPagination(pages) : null}
+				{pages ? (
+					<>
+						<h1>Showing results for: {persistentSearchQuery}</h1>
+						{renderPagination(pages)}
+						<MoviesContainer movies={movies} />
+						{renderPagination(pages)}
+					</>
+				) : (
+					<h1>{noMoviesMessage}</h1>
+				)}
 			</div>
 		</div>
 	);
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const API_KEY = "5fbae63a";
+export const STARTUP_MESSAGE = "It doesn't look like you started searching. Go on, look for a movie";
