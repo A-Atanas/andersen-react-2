2464c0bf7ac94e820f081f630e274b773c6b120f
Anthony <anton09092002@gmail.com>
2021-10-14 18:11:11 +0300

    Attempt to normalize line endings. Again

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://www.omdbapi.com/?apikey=5fbae63a";
+export const API_URL = "https://www.omdbapi.com/?apikey=5fbae63a";
 export const STARTUP_MESSAGE = "It doesn't look like you started searching. Go on, look for a movie";
